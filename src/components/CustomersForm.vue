6cfef86a86ca03b2b745afbd0b49a6697a709efb
kadonomaro <denis.afer@gmail.com>
2020-08-27 22:09:19 +0700

    feat: add vuelidate package

--- src/components/CustomersForm.vue
+++ src/components/CustomersForm.vue
@@ -3,38 +3,51 @@
 		<form class="customers-form__form" @submit.prevent="submitHandler">
 
 			<div class="customers-form__group">
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Фамилия*</span>
-					<input class="customers-form__input input" type="text" placeholder="Введите вашу фамилию" v-model="form.lastName">
+					<input class="customers-form__input input input--error" type="text" placeholder="Введите вашу фамилию" v-model="form.lastName">
+					<span class="customers-form__error">Поле обязательно для заполнения</span>
 				</label>
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Имя*</span>
-					<input class="customers-form__input input" type="text" placeholder="Введите ваше имя" v-model="form.firstName">
+					<input class="customers-form__input input input--error" type="text" placeholder="Введите ваше имя" v-model="form.firstName">
+					<span class="customers-form__error">Поле обязательно для заполнения</span>
 				</label>
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Отчество</span>
 					<input class="customers-form__input input" type="text" placeholder="Введите ваше отчество" v-model="form.patronymic">
 				</label>
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Дата рождения*</span>
-					<input class="customers-form__input input input--type-date" type="date" v-model="form.birthday">
+					<input class="customers-form__input input input--type-date input--error" type="date" v-model="form.birthday">
+					<span class="customers-form__error">Поле обязательно для заполнения</span>
 				</label>
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Телефон*</span>
-					<input class="customers-form__input input" type="tel" placeholder="Номер телефона" v-model="form.phoneNumber">
+					<input class="customers-form__input input input--error" type="tel" placeholder="Номер телефона" v-model="form.phoneNumber">
+					<span class="customers-form__error">Некорректный номер телефона</span>
 				</label>
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Пол</span>
 					<input class="customers-form__input input" type="text" placeholder="Укажите пол" v-model="form.gender">
 				</label>
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Группа клиентов*</span>
-					<select class="customers-form__input input" multiple v-model="form.customersGroup">
+					<select class="customers-form__input input input--error" multiple v-model="form.customersGroup">
 						<option value="VIP">VIP</option>
 						<option value="Проблемные">Проблемные</option>
 						<option value="ОМС">ОМС</option>
 					</select>
+					<span class="customers-form__error">Необходимо выбрать минимум 1</span>
 				</label>
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Лечащий врач</span>
 					<select class="customers-form__input input" v-model="form.doctor">
@@ -43,64 +56,84 @@
 						<option value="Чернышева">Чернышева</option>
 					</select>
 				</label>
+
 				<label class="customers-form__label">
 					<input type="checkbox" v-model="form.shortMessageDisable">
 					<span class="customers-form__title">Не отправлять СМС</span>
 				</label>
+
 			</div>
 
+
 			<div class="customers-form__group">
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Индекс</span>
 					<input class="customers-form__input input" type="text" placeholder="Введите почтовый индекс" v-model="form.postIndex">
 				</label>
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Страна</span>
 					<input class="customers-form__input input" type="tel" placeholder="Страна проживания" v-model="form.country">
 				</label>
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Область</span>
 					<input class="customers-form__input input" type="text" placeholder="Область/край в стране" v-model="form.region">
 				</label>
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Город*</span>
-					<input class="customers-form__input input" type="text" placeholder="Город проживания" v-model="form.city">
+					<input class="customers-form__input input input--error" type="text" placeholder="Город проживания" v-model="form.city">
+					<span class="customers-form__error">Поле обязательно для заполнения</span>
 				</label>
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Улица</span>
 					<input class="customers-form__input input" type="tel" placeholder="Введина название улицы" v-model="form.street">
 				</label>
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Дом</span>
 					<input class="customers-form__input input" type="text" placeholder="Введите номер дома" v-model="form.houseNumber">
 				</label>
+
 			</div>
 
+
 			<div class="customers-form__group">
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Тип документа*</span>
-					<select class="customers-form__input input" name="" v-model="form.documentType">
+					<select class="customers-form__input input input--error	" name="" v-model="form.documentType">
 						<option value="Паспорт">Паспорт</option>
 						<option value="Свидетельство о рождении">Свидетельство о рождении</option>
 						<option value="Вод. удостоверение">Вод. удостоверение</option>
 					</select>
+					<span class="customers-form__error">Вы не выбрали тип документа</span>
 				</label>
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Серия</span>
 					<input class="customers-form__input input" type="tel" placeholder="Серия документа" v-model="form.documentSeries">
 				</label>
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Номер</span>
 					<input class="customers-form__input input" type="text" placeholder="Номер документа" v-model="form.documentNumber">
 				</label>
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Кем выдан</span>
 					<input class="customers-form__input input" type="tel" placeholder="Кем выдан документ" v-model="form.documentIssuedBy">
 				</label>
+
 				<label class="customers-form__label">
 					<span class="customers-form__title">Дата выдачи*</span>
-					<input class="customers-form__input input input--type-date" type="date" v-model="form.documentIssueDate">
+					<input class="customers-form__input input input--type-date input--error" type="date" v-model="form.documentIssueDate">
+					<span class="customers-form__error">Вы не выбрали дату выдачи</span>
 				</label>
+
 			</div>
 
 			<span class="customers-form__title">* - поля обязательные для заполнения</span>
@@ -159,27 +192,36 @@ export default {
 			flex-wrap: wrap;
 			padding: 10px 0;
 			margin-bottom: 10px;
-			&:not(:last-child) {
-				border-bottom: 2px solid rgba($color: #22b2ea, $alpha: 0.5);
-			}
+			border-bottom: 2px solid rgba($color: #22b2ea, $alpha: 0.5);
 		}
 		&__label {
+			position: relative;
 			display: block;
 			max-width: 50%;
 			flex-basis: 50%;
 			padding: 0 5px;
-			margin: 0 0 10px;
+			margin: 0 0 15px;
 			box-sizing: border-box;
-		}
-		&__label--full-size {
-			max-width: 100%;
-			flex-basis: 100%;
 		}
 		&__title {
 			display: inline-block;
 			margin-bottom: 5px;
 			font-size: 12px;
 			color: #008dc5;
+		}
+		&__error {
+			position: absolute;
+			z-index: 99;
+			top: 100%;
+			left: 5px;
+			right: 5px;
+			display: block;
+			padding: 2px 5px;
+			color: #fff;
+			font-size: 12px;
+			background-color: #ff5e7b;
+			transform: translateY(-2px);
+			box-sizing: border-box;
 		}
 		&__input {
 			width: 100%;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuelidate from 'vuelidate';
 import '@/assets/css/style.scss'
 
 Vue.config.productionTip = false
+Vue.config.devtools = true
+
+Vue.use(Vuelidate)
 
 new Vue({
   render: h => h(App),
